9764422763bc374a76ace2c96a0aae5a7a27458e
Mahdiyar Salavati <mahdiyarsalavati@gmail.com>
2024-05-24 02:51:58 +0330

    add ability to add course

--- MiniProject/Admin.java
+++ MiniProject/Admin.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class Admin extends Teacher {
 
     private List<Person> teachers;
+    private List<Course> courses;
 
 
     public Admin(String firstName, String lastName, List<Course> courses, List<Person> teachers) {
@@ -13,6 +14,11 @@ public class Admin extends Teacher {
     public void addTeacher(Teacher teacher) {
         if (teacher != null && !teachers.contains(teacher))
             teachers.add(teacher);
+    }
+
+    public void addCourse(Course course) {
+        if (course != null && !courses.contains(course))
+            courses.add(course);
     }
 
 
