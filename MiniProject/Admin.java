a8e7c0e064a8a068550a4ca93a5791dd50eecd5a
Mahdiyar Salavati <mahdiyarsalavati@gmail.com>
2024-05-24 02:49:24 +0330

    defined admin as a child of teacher and added ability to add teachers

--- MiniProject/Admin.java
+++ MiniProject/Admin.java
@@ -1,2 +1,19 @@
-public class Admin {
+import java.util.List;
+
+public class Admin extends Teacher {
+
+    private List<Person> teachers;
+
+
+    public Admin(String firstName, String lastName, List<Course> courses, List<Person> teachers) {
+        super(firstName, lastName, courses);
+        this.teachers = teachers;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        if (teacher != null && !teachers.contains(teacher))
+            teachers.add(teacher);
+    }
+
+
 }
